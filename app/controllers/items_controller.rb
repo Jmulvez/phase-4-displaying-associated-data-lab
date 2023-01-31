8462481264d4f3cddc1d913b00c373fb77b633c3
John <johnmulvey48@yahoo.com>
2023-01-31 13:12:38 -0500

    git@github.com:Jmulvez/phase-4-displaying-associated-data-lab.git

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,8 @@
 class ItemsController < ApplicationController
+
+    def index
+        items = Items.all(:created_at)
+        render json: items
+    end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,8 @@
 class UsersController < ApplicationController
+
+    def show
+        user = Users.find(params[:id])
+        render json: user
+    end
+
 end
